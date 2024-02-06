fc5a647f9709344da211b11cd5c7b0eb48a72d0b
arthur santana <arthurgn.santana@gmail.com>
2024-02-06 19:34:08 -0300

    feat: criando entidades base do projeto

---
+++ ProjectAllocation.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAllocation.Domain.Entities
+{
+    public abstract class BaseEntity : BaseIdentity
+    {
+        [Column("CreatedAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("UpdatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("Inactive")]
+        public bool Inactive { get; set; }
+        public void Create(Guid userId)
+        {
+            base.Create();
+            this.CreatedAt = DateTime.Now;
+            this.Inactive = false;
+        }
+
+        public void Update(Guid userId)
+        {
+            this.UpdatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ ProjectAllocation.Domain/Entities/BaseIdentity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAllocation.Domain.Entities
+{
+    public abstract class BaseIdentity
+    {
+        [Column("Id")]
+        public Guid Id { get; set; }
+
+        public void Create()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
