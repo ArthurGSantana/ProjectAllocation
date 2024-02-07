a46578b6c863bae066416ad1480dc9431a2bdecb
arthur santana <arthurgn.santana@gmail.com>
2024-02-07 19:23:48 -0300

    feat: criando dbContext do projeto e setando suas entidades

---
+++ ProjectAllocation.Repository/Context/ProjectAllocationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectAllocation.Domain.Entities;
+namespace ProjectAllocation.Repository.Context
+{
+    public class ProjectAllocationContext : DbContext
+    {
+        public ProjectAllocationContext(DbContextOptions<ProjectAllocationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Collaborator> Collaborator { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<JobFunction> JobFunction { get; set; }
+        public DbSet<Seniority> Seniority { get; set; }
+        public DbSet<Contract> Contract { get; set; }
+        public DbSet<ContractType> ContractType { get; set; }
+        public DbSet<Allocation> Allocation { get; set; }
+        public DbSet<AllocationStatus> AllocationStatus { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<ProjectStatus> ProjectStatus { get; set; }
+        public DbSet<Profile> Profile { get; set; }
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<ProfilePermission> ProfilePermission { get; set; }
+    }
+}
