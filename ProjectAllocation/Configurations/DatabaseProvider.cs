10faab733c23fb9097f682d4943f7ecc39157dd2
arthur santana <arthurgn.santana@gmail.com>
2024-02-07 19:04:50 -0300

    feat: adicionando string de conexao com o postgres e implementando provider para fazer seu retorno

---
+++ ProjectAllocation/Configurations/DatabaseProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectAllocation.API.Configurations
+{
+    public class DatabaseProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetConnectionString()
+        {
+            var usernameDatabase = Environment.GetEnvironmentVariable("DATABASE_USERNAME");
+            var passwordDatabase = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+
+            var connString = _configuration.GetConnectionString("DatabasePostgres")
+                .Replace("{DATABASE_USERNAME}", usernameDatabase)
+                .Replace("{DATABASE_PASSWORD}", passwordDatabase);
+
+            return connString;
+        }
+    }
+}
