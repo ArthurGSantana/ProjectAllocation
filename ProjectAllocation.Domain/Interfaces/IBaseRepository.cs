1373bd41e231849e1a8ffbd1905fdda9c0405e90
arthur santana <arthurgn.santana@gmail.com>
2024-02-25 09:22:24 -0300

    feat: implementando interface e metodos do repositorio base

---
+++ ProjectAllocation.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProjectAllocation.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace ProjectAllocation.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseIdentity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<T?> GetByExpressionAsync(Expression<Func<T, bool>> expression);
+        Task<T?> GetIncludesByExpressionAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllByExpressionAsync(Expression<Func<T, bool>> expression);
+        Task<IEnumerable<T>> GetAllIncludesByExpressionAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes);
+        T Add(T entity);
+        List<T> AddRange(List<T> entities);
+        void Update(T entity);
+        void UpdateRange(List<T> entities);
+        void Delete(T entity);
+    }
+}
---
+++ ProjectAllocation.Repository/Repository/BaseRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectAllocation.Domain.Entities;
+using ProjectAllocation.Domain.Interfaces;
+using System.Linq.Expressions;
+
+namespace ProjectAllocation.Repository.Repository
+{
+    internal class BaseRepository<T> : IBaseRepository<T> where T : BaseIdentity
+    {
+        protected readonly DbContext _dbContext;
+
+        public BaseRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<T?> GetByExpressionAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbContext.Set<T>().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<T?> GetIncludesByExpressionAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes)
+        {
+            return await includes.Aggregate(_dbContext.Set<T>().Where(expression).AsQueryable(), (current, include) => current.Include(include)).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllByExpressionAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbContext.Set<T>().AsNoTracking().Where(expression).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllIncludesByExpressionAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes)
+        {
+            return await includes.Aggregate(_dbContext.Set<T>().Where(expression).AsQueryable(), (current, include) => current.Include(include)).ToListAsync();
+        }
+
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+
+            return entity;
+        }
+
+        public List<T> AddRange(List<T> entities)
+        {
+            _dbContext.Set<T>().AddRange(entities);
+
+            return entities;
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+        }
+
+        public void UpdateRange(List<T> entities)
+        {
+            _dbContext.Set<T>().UpdateRange(entities);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+    }
+}
